8641b34596fdc05bd5492917d45e46fd80171b1d
Harshil Enuguru <115143239+HarshilE@users.noreply.github.com>
2024-06-06 08:46:52 +0530

    VM_Pub variables.tf completed

--- modules/VM/Pub_VM/variables.tf
+++ modules/VM/Pub_VM/variables.tf
@@ -1,1 +1,43 @@
 
+variable "resource_group_name" {
+    description = "name of the resource group"
+    type = string
+}
+
+variable "location" {
+    description = "Location of the resources created"
+    type = string
+}
+
+variable "size" {
+    description = "size of the instance"
+    type = string
+}
+
+variable "network_interface_ids" {
+    description = "VNet output value"
+}
+
+variable "admin_username" {
+    description = "name of the admin user"
+}
+
+variable "username" {
+    description = "username specified" 
+}
+
+variable "key_filepath" {
+  description = "key-pair file path"
+}
+
+variable "publisher" {
+    description = "publisher detials"  
+}
+
+variable "offer" {
+    description = "offer details"  
+}
+
+variable "sku" {
+    description = "sku details"
+}
